a01451b487d4a131b0d07db94eff1a3dda368df6
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-07-17 18:23:06 -0300

    Delete old utils

--- utils/enum/Estado.js
+++
@@ -1,5 +0,0 @@
-export default {
-  VIGENTE: 'VIGENTE',
-  ELIMINADO: 'ELIMINADO',
-  HABILITADO: 'HABILITADO',
-}
---
+++ utils/enum/UsuarioRoles.js
@@ -0,0 +1,9 @@
+/**
+ * Tipos de usuario
+ * @readonly
+ * @enum {string}
+ */
+export default {
+  ADMIN: 'ADMIN',
+  PROPIETARIO: 'PROPIETARIO',
+}
