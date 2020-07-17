baec80dd3cb940edcc8c14e45c21e3819b059cb1
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-07-17 15:26:08 -0300

    Fixed load project

--- pages/inicio.vue
+++ pages/inicio.vue
@@ -1,115 +1,14 @@
 <template>
   <v-layout column justify-center pb-12 mb-2 mt-4 px-2>
-    <!-- Alert message -->
-    <v-layout v-if="isSomeOfficeHoliday" justify-center fill-height>
-      <v-flex xs12 lg8>
-        <v-alert
-          text
-          dense
-          border="left"
-          :type="isAllOfficeHoliday ? 'error' : 'warning'"
-        >
-          <p v-if="isAllOfficeHoliday" class="mb-0">
-            El establecimiento se encuentra cerrado.
-          </p>
-          <v-layout v-for="(suc, key) in someOfficeHoliday" :key="key" column>
-            <p
-              v-show="isSomeOfficeHoliday !== 1"
-              class="mb-0 title"
-              v-text="suc.descripcion"
-            />
-            <p class="mb-0" v-text="suc.mensajeVacaciones" />
-          </v-layout>
-        </v-alert>
-      </v-flex>
-    </v-layout>
-    <v-layout v-if="isPendiente" justify-center fill-height>
-      <v-flex xs12 lg8>
-        <v-alert text dense border="left" colored-border type="warning">
-          <p class="mb-0">
-            Cuenta Profesor pendiente de validación. Por favor valide su cuenta
-            presencialmente en la fotocopiadora.
-          </p>
-        </v-alert>
-      </v-flex>
-    </v-layout>
-    <!-- Logo -->
-    <v-flex xs12 md10 lg10 xl6 mx-3 mb-3 mt-4 class="text-center">
-      <v-flex xs12>
-        <img
-          src="/img/logo_rapicopia.png"
-          alt="logo de rapicopia"
-          :style="{ 'max-width': logoHeight }"
-          width="100%"
-          height="100%"
-        />
-      </v-flex>
-    </v-flex>
-
-    <v-layout justify-center fill-height>
-      <v-flex xs12 md11 lg11 xl6>
-        <v-layout column justify-center>
-          <v-flex xs12 pb-2>
-            <p class="title my-2">Buscar apuntes</p>
-            <!-- Busqueda -->
-            <SearchBar advanced-search />
-            <!-- Subit archivo -->
-          </v-flex>
-          <v-flex xs12>
-            <p class="title my-2">Agrega archivos pdf a tu pedido</p>
-            <UploadFile key-grup="CART" multiple class="mx-4" />
-          </v-flex>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+    Cliente page
   </v-layout>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import SearchBar from '../components/SearchBar'
-import UploadFile from '../components/UploadFile'
-import { EstadoUsuario } from '../utils/enum'
-
-function logoHeight(name) {
-  switch (name) {
-    case 'xs':
-      return '150px'
-    case 'sm':
-      return '180px'
-    case 'md':
-      return '200px'
-    default:
-      return '294px'
-  }
-}
-
 export default {
   middleware: ['authenticated', 'auth_cliente'],
-  components: { SearchBar, UploadFile },
-  asyncData({ $vuetify, store }) {
-    store.commit('setDrawer', $vuetify.breakpoint.mdAndUp)
-    return {
-      logoHeight: logoHeight($vuetify.breakpoint.name),
-    }
-  },
-  computed: {
-    ...mapState(['user']),
-    ...mapState('sucursales', ['sucursales']),
-    isPendiente() {
-      return this.user.estadoUsuario === EstadoUsuario.PENDIENTE
-    },
-    isAllOfficeHoliday() {
-      return this.sucursales.every((s) => s.onVacation)
-    },
-    someOfficeHoliday() {
-      return this.sucursales.filter(
-        (s) => s.fourDaysLeftToVaction || s.onVacation
-      )
-    },
-    isSomeOfficeHoliday() {
-      return this.someOfficeHoliday.length
-    },
-  },
+  components: {},
+  asyncData() {},
+  computed: {},
 }
 </script>
--- pages/loginout.vue
+++ pages/loginout.vue
@@ -2,14 +2,6 @@
   <v-container class="fill-height" fluid>
     <v-row align="center" justify="center" class="mb-12">
       <div class="text-center">
-        <img
-          src="/img/logo_rapicopia.png"
-          alt="logo de rapicopia"
-          style="max-width: 256px;"
-          width="100%"
-          height="100%"
-          class="mb-6"
-        />
         <h1>Adiós</h1>
         <p class="description mt-4">
           <NuxtLink to="/">Ir al inicio</NuxtLink>
@@ -29,11 +21,11 @@ import { mapActions } from 'vuex'
 
 export default {
   async created() {
-    await this.signOut()
+    await this.logout()
     this.$router.replace('/')
   },
   methods: {
-    ...mapActions(['signOut']),
+    ...mapActions(['logout']),
   },
 }
 </script>
